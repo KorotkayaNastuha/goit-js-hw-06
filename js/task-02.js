28f2749c304448502fd4a537d8c537e5305d8a9a
KorotkayaNastuha <skiteikina11@gmail.com>
2022-09-17 23:26:39 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+function addIngredients(ingredients) {
+  let ingredientsList = []
+  for (const ingredient of ingredients) {
+    const ingredientsEl = document.createElement('li');
+    ingredientsEl.classList.add('item');
+    ingredientsEl.textContent = ingredient;
+    ingredientsList.push(ingredientsEl);
+  };
+  return document.querySelector('#ingredients').append(...ingredientsList)
+}
+(addIngredients(ingredients))
+ 
