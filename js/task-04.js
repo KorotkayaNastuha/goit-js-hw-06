21c04d845f4304da283e7e394bab41dd20517784
KorotkayaNastuha <skiteikina11@gmail.com>
2022-09-19 18:44:31 +0300

    task-4

---
+++ js/task-04.js
@@ -0,0 +1,9 @@
+const counterValue = 0;
+function decrement(){
+	counterValue -=1 ;
+  document.getElementById('value').innerHTML = counterValue;
+}
+function increment(){
+	counterValue += 1;
+  document.getElementById('value').innerHTML = counterValue;
+}
