0642cd96a44d964c24645ddb62b5440db37bf619
KorotkayaNastuha <skiteikina11@gmail.com>
2022-09-20 22:51:17 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+    console.log(email.value, password.value);
+    if (email.value === '' || password.value === '') {
+        return alert('All fields must be hidden');
+    }
+    event.currentTarget.reset()
+});
