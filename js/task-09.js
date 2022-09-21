02561dc1b2d7e3c72b7ab0e6b3ad1b01443b5e05
KorotkayaNastuha <skiteikina11@gmail.com>
2022-09-21 20:08:47 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const buttonEl = document.querySelector('.change-color');
+const colorEl = document.querySelector('.color');
+const body = document.querySelector('body')
+
+buttonEl.addEventListener('click', onChangeColor)
+function onChangeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = body.style.backgroundColor;
+}
