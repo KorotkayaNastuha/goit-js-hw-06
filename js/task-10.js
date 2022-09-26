b97dcf63f24e05d2901e6204b5faf14bbc6b3c20
KorotkayaNastuha <skiteikina11@gmail.com>
2022-09-26 21:41:52 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const createEl = document.querySelector('[data-create]');
+const destroyEl = document.querySelector('[data-destroy]');
+const boxesEl = document.getElementById('#boxes');
+const amountEl = document.querySelector('#controls > input');
+
+createEl.addEventListener('click', createBoxes);
+destroyEl.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const divElemToAdd = [];
+  amount = amountEl.value
+  for (let i = 0; i < amount; i+=1) {
+    const elem = document.createElement('div');
+    elem.style.background = getRandomHexColor()
+    elem.style.height = `{30 + 10}px`
+    elem.style.weight = `{30 + 10}px`
+    divElemToAdd.push(div)
+  }
+  return divElemToAdd
+}
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+  amountEl.value = 0;
+}
